b7cd28f71df477ac6a1e90c96154e59edbbf2d93
Juan Prieto <162722098+juanp-r@users.noreply.github.com>
2024-08-29 21:38:30 -0300

    App.java

--- src/main/java/com/template/App.java
+++ src/main/java/com/template/App.java
@@ -5,60 +5,87 @@ public class App {
     // 1. Sum of Two Integers
     public int sum(int a, int b) {
         // TODO: Implement this method
-        return 0;
+        return a+b;
     }
 
     // 2. Check Even or Odd
     public boolean isEven(int number) {
         // TODO: Implement this method
-        return false;
+        if (number % 2 == 0){
+            return true;
+        }
+            return false;
     }
 
     // 3. Maximum of Two Numbers
     public int max(int a, int b) {
         // TODO: Implement this method
-        return 0;
+        if (a > b){
+            return a;
+        }
+        return b;
     }
 
     // 4. Factorial of a Number
     public int factorial(int n) {
         // TODO: Implement this method
-        return 0;
+        if (n<=1) {
+            return 1;
+        }
+        return n*factorial(n-1);
     }
 
     // 5. Count Characters in a String
     public int countChars(String input) {
         // TODO: Implement this method
-        return 0;
+        return input.length();
     }
 
     // 6. Reverse a String
     public String reverse(String input) {
         // TODO: Implement this method
-        return "";
+        if (input.isEmpty()) {
+            return input;
+        }
+        return reverse(input.substring(1)) + input.charAt(0);
+
     }
 
     // 7. Check Prime Number
     public boolean isPrime(int number) {
-        // TODO: Implement this method
-        return false;
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     // 8. Find the Smallest Element in an Array
     public int findMin(int[] array) {
-        // TODO: Implement this method
-        return 0;
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
     }
 
     // 9. Sum of Elements in an Array
     public int arraySum(int[] array) {
-        // TODO: Implement this method
-        return 0;
+        int sum = 0;
+        for (int i : array) {
+            sum += i;
+        }
+        return sum;
     }
 
     // 10. Convert Celsius to Fahrenheit
     public double celsiusToFahrenheit(double celsius) {
-        // TODO: Implement this method
-        return 0.0;
+        return (celsius * 9 / 5) + 32;
     }
 }
